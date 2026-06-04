94b6f88e1ec700450cc64f5d5671bdb38d41d762
layasowmya <arunkumarsundaresan@gmail.com>
2026-06-03 20:41:08 -0400

    Add function to save predictions in index.js

--- predict/index.js
+++ predict/index.js
@@ -1,1 +1,9 @@
+module.exports = async function (context, req) {
+  const prediction = req.body;
+  context.log("Saving prediction:", prediction);
+  context.res = {
+    status: 200,
+    body: { message: "Prediction saved successfully!" }
+  };
+};
 
